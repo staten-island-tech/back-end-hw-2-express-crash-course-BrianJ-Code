94ee50eb6533d2b494b386140e67dcd95dae6850
BrianJ-Code <55200248+BrianJ-Code@users.noreply.github.com>
2020-01-13 22:34:54 -0500

    1

---
+++ Express/Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+
+    },
+    {
+        id: 2,
+        name: 'Joe Mama',
+        email: 'joe@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Yeet Yeet',
+        email: 'yeet@gmail.com',
+        status: 'active'
+    },
+];
+module.exports = members;
